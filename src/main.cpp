35471d6535847b6954f86f78b0eb02d131a8f3b1
JacksonK <jackmkirchner@gmail.com>
2025-05-27 22:03:36 -0500

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+
+    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "Flag Football");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    window.setFramerateLimit(60);
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+            {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+}
